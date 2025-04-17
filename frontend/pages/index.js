913550635d8d58651b9e36d72f56047bc530b0a3
Matias.Martinez <34101826+MatiasMartinez90@users.noreply.github.com>
2025-04-17 19:04:12 -0300

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,23 @@
+// /pages/index.js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useAuth } from '../contexts/AuthContext';
 
+export default function Home() {
+  const { isAuthenticated, loading } = useAuth();
+  const router = useRouter();
+  
+  useEffect(() => {
+    if (!loading && !isAuthenticated) {
+      router.push('/login');
+    } else if (!loading && isAuthenticated) {
+      router.push('/dashboard');
+    }
+  }, [isAuthenticated, loading, router]);
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-2xl">Cargando...</h1>
+    </div>
+  );
+}
